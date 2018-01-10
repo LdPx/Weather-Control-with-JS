122cb89eebb289f5d1132c3e53760f78147d0382
DieGabelLinksVomTeller <thomas.schell35@web.de>
2018-01-10 10:31:17 +0100

    added control dependencies

---
+++ src/controls.js
@@ -0,0 +1,15 @@
+
+function createControls(camera){
+    var controls = new THREE.TrackballControls(camera);
+    controls.rotateSpeed = 1.0;
+    controls.zoomSpeed = 10.2;
+    controls.panSpeed = 0.8;
+    controls.noZoom = false;
+    controls.noPan = false;
+    controls.staticMoving = true;
+    controls.dynamicDampingFactor = 0.3;
+    // a, s, d
+    controls.keys = [65, 83, 68];
+    //controls.addEventListener('change', render);
+    return controls;
+}
